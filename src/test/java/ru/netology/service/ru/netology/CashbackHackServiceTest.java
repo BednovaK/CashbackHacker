ad287cb1ed2904de712b3f5e7e2fea6395a8f47b
Bednova Ekaterina <96745291+BednovaK@users.noreply.github.com>
2022-04-23 15:15:39 +0300

    Tests added

---
+++ src/test/java/ru/netology/service/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service.ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int expected = 200;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain4() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+}
